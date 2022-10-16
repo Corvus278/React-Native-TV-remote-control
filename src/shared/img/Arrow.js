16f2e8ff561efa87f5f77fdecdeca03530722b16
Corvus278 <makar.korolenko2005@mail.ru>
2022-10-16 16:10:46 +0300

    Add some icons

---
+++ src/shared/img/Arrow.js
@@ -0,0 +1,19 @@
+import { Svg, Path } from 'react-native-svg';
+
+export const Arrow = () => {
+	return (
+		<Svg
+			width='11'
+			height='7'
+			viewBox='0 0 11 7'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<Path
+				d='M0.75 6L4.87835 1.41294C5.07697 1.19225 5.42303 1.19225 5.62165 1.41294L9.75 6'
+				stroke='#A2A2A2'
+				stroke-linecap='round'
+			/>
+		</Svg>
+	);
+};
---
+++ src/shared/img/Home.js
@@ -0,0 +1,19 @@
+import { Svg, Path } from 'react-native-svg';
+
+export const Home = () => {
+	return (
+		<Svg
+			width='16'
+			height='16'
+			viewBox='0 0 16 16'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<Path
+				d='M14.5 15.5H2C1.44772 15.5 1 15.0523 1 14.5V6.5251C1 6.19632 1.16161 5.88854 1.43227 5.70188L7.68227 1.39154C8.02406 1.15582 8.47594 1.15582 8.81773 1.39154L15.0677 5.70188C15.3384 5.88854 15.5 6.19632 15.5 6.5251V14.5C15.5 15.0523 15.0523 15.5 14.5 15.5Z'
+				stroke='#A2A2A2'
+				stroke-linecap='round'
+			/>
+		</Svg>
+	);
+};
